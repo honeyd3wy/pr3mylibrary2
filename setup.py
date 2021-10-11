1f4a6bb027656723730636d42345fc54eaaf7d70
honeyd3wy <holyshyours@naver.com>
2021-10-11 22:34:19 +0900

    uploaded

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author = 'honeyd3wy',
     author_email = 'holysjy0410@gmail.com',
     packages = ['pr3mylibrary2'],
-    py_modules=['data_handling', 'model'],
+    py_modules=['data_handling'],
     install_requires=['aiohttp==3.7.4.post0',
     'aniso8601==9.0.1',
     'async-timeout==3.0.1',
